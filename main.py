4344754cf209be607a3ad36bb6b2e6c06eea2604
Yuliia Zahoruulko <yzahorulko@gmail.com>
2026-02-12 13:53:13 +0200

    covered with code

--- main.py
+++ main.py
@@ -1,7 +1,78 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+
+    genres = [
+        "Western",
+        "Action",
+        "Dramma"
+    ]
+    actors = [
+        "George Klooney",
+        "Kianu Reaves",
+        "Scarlett Keegan",
+        "Will Smith",
+        "Jaden Smith",
+        "Scarlett Johansson"
+    ]
+    actors_as_tuples = [(actor.split(" ")[0], actor.split(" ")[1]) for actor in actors]
+
+    for genre in genres:
+        Genre.objects.create(
+            name=genre
+        )
+
+    for actor in actors_as_tuples:
+        Actor.objects.create(
+            first_name=actor[0],
+            last_name=actor[1]
+        )
+
+    genres_to_update = {
+        'Dramma': "Drama"
+    }
+    actors_to_update = {
+        ("George", "Klooney"): ["George", "Clooney"],
+        ("Kianu", "Reaves"): ["Keanu", "Reeves"]
+    }
+
+    for genre_key, genre_value in genres_to_update.items():
+        Genre.objects.filter(
+            name=genre_key
+        ).update(name=genre_value)
+    for actor_key, actor_value in actors_to_update.items():
+        Actor.objects.filter(
+            first_name=actor_key[0],
+            last_name=actor_key[1]
+        ).update(
+            first_name=actor_value[0],
+            last_name=actor_value[1]
+        )
+
+    genre_to_delete = ["Action"]
+    actor_to_delete = [("Scarlett", ""),]
+    for genre in genre_to_delete:
+        Genre.objects.filter(
+            name=genre
+        ).delete()
+    for actor in actor_to_delete:
+        if actor[0] != "":
+            Actor.objects.filter(
+                first_name=actor[0]
+            ).delete()
+        if actor[1] != "":
+            Actor.objects.filter(
+                last_name=actor[1]
+            ).delete()
+
+    return Actor.objects.filter(
+        last_name="Smith"
+    ).order_by("first_name")
+
+
+if __name__ == "__main__":
+    main()
